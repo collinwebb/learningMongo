ee9bb8e7f4329982a381efa1ff2e8f809d1e32be
Collin Webb <collindwebb@gmail.com>
2015-07-06 09:19:51 -0700

    fixes access control

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
